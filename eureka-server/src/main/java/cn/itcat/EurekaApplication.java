f0c6463181fdc8dcdf779f96fb2c99bbac887b34
zyroot <1209056907@qq.com>
2019-02-20 10:53:06 +0800

    单台eureka整合

---
+++ eureka-server/src/main/java/cn/itcat/EurekaApplication.java
@@ -0,0 +1,17 @@
+package cn.itcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by Zy on 2019/2/20.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
